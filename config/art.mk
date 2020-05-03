d67df5c96f18811d629bb4c404229c2b1686bdaa
Inventor1938 <ivan.kolev@mail.bg>
2020-05-03 20:50:18 +0300

    1
    Change-Id: I65e0b57e8b77bf31a5842e23f484847ea99ba940

--- config/art.mk
+++ config/art.mk
@@ -9,8 +9,10 @@ USE_DEX2OAT_DEBUG := false
 # Dex pre-opt
 WITH_DEXPREOPT := true
 WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := false
-PRODUCT_DEX_PREOPT_PROFILE_DIR := vendor/dexopt_profiles
-PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := speed-profile
+#PRODUCT_DEX_PREOPT_PROFILE_DIR := vendor/dexopt_profiles
+PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := everything
+PRODUCT_DEX_PREOPT_BOOT_FLAGS := --compiler-filter=everything
+PRODUCT_DEX_PREOPT_DEFAULT_FLAGS := --compiler-filter=everything
 
 # Boot image profile
 PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
@@ -19,18 +21,15 @@ PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-im
 # System server compiler
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
 
-# Dexopt boot types
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    pm.dexopt.first-boot=extract \
-    pm.dexopt.boot=verify
-
 # Dexopt filters
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    pm.dexopt.install=speed-profile \
-    pm.dexopt.bg-dexopt=speed-profile \
-    pm.dexopt.ab-ota=speed-profile \
+    pm.dexopt.first-boot=everything \
+    pm.dexopt.boot=everything \
+    pm.dexopt.install=everything \
+    pm.dexopt.bg-dexopt=everything \
+    pm.dexopt.ab-ota=everything \
     pm.dexopt.inactive=verify \
-    pm.dexopt.shared=speed
+    pm.dexopt.shared=everything
 
 # dex2oat threads (default)
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
