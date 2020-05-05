df87d855120613f53d1f5fb4ea964aa882f3685c
Inventor1938 <ivan.kolev@mail.bg>
2020-05-05 03:35:42 +0300

    1
    Change-Id: Ib742d312c00dacb41022e92963d9d0eb02f17b82

--- config/art.mk
+++ config/art.mk
@@ -19,7 +19,7 @@ PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
 PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
 
 # System server compiler
-PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := everything-profile
 
 # Dexopt filters
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
